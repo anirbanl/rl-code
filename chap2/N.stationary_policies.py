d9e1269510b0ea1ccf8d1a8b5bcc4476d046e1f7
anirbanl <anirbanlaha@gmail.com>
2019-09-11 23:57:12 +0530

    Added UCB algorithm

--- chap2/N.stationary_policies.py
+++ chap2/N.stationary_policies.py
@@ -37,6 +37,47 @@ def run_eps_greedy_alpha(qstar, initval=0, runs=2000, steps=1000, eps=0.0, k=10,
         rewardlist.append(l)
     return rewardlist
 
+def run_upper_confidence_bound(qstar, initval=0, runs=2000, steps=1000, eps=0.0, k=10, alpha='avg', c=2):
+    rewardlist = []
+    for run in range(runs):
+        l = []
+        qest = np.zeros(k)+initval
+        count = np.zeros(k)
+        rewardsum = np.zeros(k)
+        action = np.random.randint(0, k)
+        count[action] += 1
+        qvalues = qstar[run]
+        reward = np.random.normal(qvalues[action], 1)
+        rewardsum[action] += reward
+        l.append((action, reward))
+        if alpha=='avg':
+            qest[action] = rewardsum[action] * 1.0 / count[action]
+        else:
+            qest[action] = qest[action] + alpha * (reward - qest[action])
+        prob = np.random.uniform(0, 1, steps - 1)
+        for step in range(steps - 1):
+            min_count_indices = np.where(count == count.min())[0]
+            num_min = min_count_indices.shape[0]
+            random_min_index = min_count_indices[np.random.randint(0, num_min)]
+            if count[random_min_index] == 0:
+                action_max = random_min_index
+            else:
+                action_max = np.argmax(qest + c * np.sqrt(np.log(step+1)/count))
+            action_rand = np.random.randint(0, k)
+            action = action_rand
+            if prob[step] > eps:
+                action = action_max
+            count[action] += 1
+            reward = np.random.normal(qvalues[action], 1)
+            rewardsum[action] += reward
+            l.append((action, reward))
+            if alpha == 'avg':
+                qest[action] = rewardsum[action] * 1.0 / count[action]
+            else:
+                qest[action] = qest[action] + alpha * (reward - qest[action])
+        rewardlist.append(l)
+    return rewardlist
+
 def compute_average_reward(rewardlist, runs=2000, steps=1000):
     rewardarray = np.zeros((runs, steps), dtype=np.float)
     for run in range(runs):
@@ -61,7 +102,7 @@ def define_qstar(runs, k):
     return qstar
 
 if __name__== "__main__":
-    runs_list = ['greedy', '0.01greedy', '0.1greedy', 'opt_greedy', 'opt_greedy_0.1alpha', '0.1greedy_0.1alpha']
+    runs_list = ['greedy', '0.01greedy', '0.1greedy', 'opt_greedy', 'opt_greedy_0.1alpha', '0.1greedy_0.1alpha', 'UCB']
     plots = []
     global_steps = 1000
 
@@ -112,6 +153,15 @@ if __name__== "__main__":
     a = compute_average_reward(r, runs, steps)
     o = count_optimal_action_selection(qstar, r, runs, steps)
     plots.append(([runs, steps, eps, k, alpha], [a, o], runs_list[5]))
+
+    ##### Run SEVEN - UCB with c=2 and eps = 0
+    runs, steps, eps, k, alpha = 2000, global_steps, 0.0, 10, 'avg'
+    initval = 0
+    r = run_upper_confidence_bound(qstar, initval, runs, steps, eps, k, alpha, c=2)
+    a = compute_average_reward(r, runs, steps)
+    o = count_optimal_action_selection(qstar, r, runs, steps)
+    plots.append(([runs, steps, eps, k, alpha], [a, o], runs_list[6]))
+
 
     t=[]
     for a,b,c in plots:
